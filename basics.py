3b69d3b9fee9df4c794f1f610f1ba3e08e7d7f52
William G Underwood <42812654+WGUNDERWOOD@users.noreply.github.com>
2024-09-03 10:16:31 +0100

    File

---
+++ basics.py
@@ -0,0 +1,25 @@
+# Types and lists
+a = 5
+print(a)
+
+b = 0.1
+print(b)
+
+c = 2
+print(float(c))
+
+hello = "Hello"
+print(hello)
+
+world = "world"
+hello_world = hello + ", " + world + "!"
+print(hello_world)
+
+fruits = ["apple", "banana", "cherry"]
+
+# Functions
+
+# Modules
+# If statements
+# For loops
+# List comprehension
